db52ccc405d0475d216a6fdca569a8a8b11d6b20
jeffzhang <jeffzh3ng>
2018-07-13 16:57:25 +0800

    Update: subdoamin scanner multi-process change to multiple coroutine

--- fuxi/views/lib/parse_target.py
+++ fuxi/views/lib/parse_target.py
@@ -13,11 +13,11 @@ def parse_target(host_list):
     result_list = []
     for host in host_list:
         host = host.strip()
-        re_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
-        re_ips = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$')
-        re_url = re.compile('[^\s]*.[a-zA-Z]')
-        re_ip_port = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$')
-        re_url_port = re.compile('[^\s]*.[a-zA-Z]:\d{1,5}')
+        re_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')  # IP
+        re_ips = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$')  # IPs
+        re_url = re.compile('[^\s]*.[a-zA-Z]')  # URL
+        re_ip_port = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$')  # IP + Port
+        re_url_port = re.compile('[^\s]*.[a-zA-Z]:\d{1,5}')  # URL + Port
         if re_ip.match(host):
             result_list.append(host)
         elif re_ips.match(host):
--- fuxi/views/modules/subdomain/domain_brute.py
+++ fuxi/views/modules/subdomain/domain_brute.py
@@ -5,9 +5,12 @@
 # @File    : domain_brute.py
 # @Desc    : ""
 
-
-import dns.resolver
-from multiprocessing import Pool, Lock
+import socket
+import gevent
+import time
+import threading
+from gevent import monkey
+from gevent import pool as gevent_pool
 from datetime import datetime
 from random import sample
 from string import digits, ascii_lowercase
@@ -15,125 +18,126 @@ from fuxi.views.lib.mongo_db import connectiondb, db_name_conf
 from fuxi.views.lib.get_title import get_title
 from instance import config_name
 
-lock = Lock()
+monkey.patch_all()
+socket.setdefaulttimeout(5)
 domain_db = db_name_conf()['domain_db']
 config_db = db_name_conf()['config_db']
 subdomain_db = db_name_conf()['subdomain_db']
 
 
-def resolution(domain):
-    _result = {}
-    record_a = []
-    record_cname = []
+def start_domain_brute(domain_list, domain_id):
+    time_start = datetime.now()
+    print("[*] %s Domain Brute start %s" % (time_start.strftime("%Y-%m-%d %H:%M:%S"), domain_id))
+    time.sleep(5)  # Don't delete it
+    connectiondb(domain_db).update_one({"_id": domain_id}, {"$set": {
+        "status": "Running"
+    }})
     try:
-        respond = dns.resolver.query(domain.strip())
-        for record in respond.response.answer:
-            for i in record.items:
-                if i.rdtype == dns.rdatatype.from_text('A'):
-                    record_a.append(str(i))
-                    _result[domain] = record_a
-                elif i.rdtype == dns.rdatatype.from_text('CNAME'):
-                    record_cname.append(str(i))
-                    _result[domain] = record_cname
+        for domain in domain_list:
+            scanner = DomainBrute(domain, domain_id)
+            scanner.start()
     except Exception as e:
-        # print(e)
-        pass
-    return _result
+        print("[!] Traversal error %s" % e)
+    connectiondb(domain_db).update_one({"_id": domain_id}, {"$set": {
+        "status": "Done"
+    }})
+    time_end = datetime.now()
+    print("[*] %s Domain Brute Done %s" % (time_end.strftime("%Y-%m-%d %H:%M:%S"), domain_id))
+    print("[*] %s Used Time: %s" % (time_end.strftime("%Y-%m-%d %H:%M:%S"), (time_end - time_start).seconds))
+
+
+# def get_domain_title(domain_id):
+#     pool = Pool(processes=50)
+#     result = []
+#     for i in connectiondb(subdomain_db).find({"domain_id": domain_id}):
+#         result.append(pool.apply_async(get_title, (i['subdomain'], i['_id'])))
+#     pool.close()
+#     pool.join()
+#     for res in result:
+#         lock.acquire()
+#         try:
+#             connectiondb(subdomain_db).update_one({"_id": res.get()["_id"]}, {"$set": {
+#                 "title": res.get()['title']
+#             }})
+#         except Exception as e:
+#             print("update title error", e)
+#         lock.release()
 
 
 class DomainBrute:
 
     def __init__(self, domain, domain_id):
-        self.domain = domain
+        print("[*] %s %s Brute Start" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), domain))
+        self.domain = domain.strip()
         self.domain_id = domain_id
-        self.sub_domain = []
-        self.third_domain = connectiondb(domain_db).find_one({"_id": domain_id})['third_domain']
-        self.resolver_ip = ''
-        self.result = ''
+        self.sub_dict = connectiondb(config_db).find_one({"config_name": config_name})['subdomain_dict_2']
+        self.next_sub_dict = connectiondb(config_db).find_one({"config_name": config_name})['subdomain_dict_3']
+        self.check_domain_list = []
         self.thread = int(connectiondb(config_db).find_one({"config_name": config_name})['subdomain_thread'])
-        self.subdomain_dict_2 = connectiondb(config_db).find_one({"config_name": config_name})['subdomain_dict_2']
-        self.subdomain_dict_3 = connectiondb(config_db).find_one({"config_name": config_name})['subdomain_dict_3']
+        self.next_domain_opt = connectiondb(domain_db).find_one({"_id": domain_id})['third_domain']
         self.random_subdomain = ''.join(sample(digits + ascii_lowercase, 10)) + '.' + domain
+        self.resolver_domain = ''
+        self.res_tmp = []
 
-    def domain_handle(self):
-        for sub_domain_2 in self.subdomain_dict_2:
-            self.sub_domain.append(sub_domain_2.strip() + '.' + self.domain)
-        if self.third_domain == "Enable":
-            for sub_domain_3 in self.subdomain_dict_3:
-                for sub_domain_2 in self.subdomain_dict_2:
-                    sub_domain = sub_domain_3 + "." + sub_domain_2
-                    self.sub_domain.append(sub_domain.strip() + '.' + self.domain)
-        return self.sub_domain
+    def _domain_parse(self):
+        for sub in self.sub_dict:
+            check_domain = sub + '.' + self.domain
+            self.check_domain_list.append(check_domain)
+        if self.next_domain_opt == 'Enable':
+            for next_sub in self.next_sub_dict:
+                for sub in self.sub_dict:
+                    next_check_domain = next_sub + '.' + sub + '.' + self.domain
+                    self.check_domain_list.append(next_check_domain)
 
-    def resolver_check(self):
+    def start(self):
+        self._domain_parse()
+        self._get_resolver_domain()
+        jobs = []
+        p = gevent_pool.Pool(self.thread)
+        for subdomain in self.check_domain_list:
+            jobs.append(p.spawn(self.gethost, subdomain))
+        gevent.joinall(jobs)
+        print("[*] %s %s Brute Done" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self.domain))
+        self._result_save()
+
+    def _get_resolver_domain(self):
         try:
-            var = resolution(self.random_subdomain)
-            if var[self.random_subdomain]:
-                return var[self.random_subdomain]
-            else:
-                return False
+            host = socket.gethostbyname(self.random_subdomain)
+            if host:
+                self.resolver_domain = host
         except Exception as e:
             # print(e)
-            return False
+            pass
 
-    def multi_brute(self):
-        start_date = datetime.now()
-        self.resolver_ip = self.resolver_check()
-        pool = Pool(processes=self.thread)
-        result = []
-        for sub_domain in self.domain_handle():
-            result.append(pool.apply_async(resolution, (sub_domain,)))
-        pool.close()
-        pool.join()
-        for res in result:
-            self.result = res.get()
-            for subdomain in self.result:
-                if self.result[subdomain] != self.resolver_ip:
-                    data = {
-                        "subdomain": subdomain,
-                        "domain": self.domain,
-                        "domain_id": self.domain_id,
-                        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        "result": self.result[subdomain],
-                        "title": '',
-                    }
-                    lock.acquire()
-                    try:
-                        connectiondb(subdomain_db).insert_one(data)
-                        # print(self.result, var)
-                    except Exception as e:
-                        print("save_db error", e)
-                    lock.release()
-        scan_time = datetime.now() - start_date
-        print("++++++++++ Scan Done! ++++++++++", scan_time.total_seconds())
-
-
-def start_domain_brute(domain, domain_id):
-    connectiondb(domain_db).update_one({"_id": domain_id}, {"$set": {
-        "status": "Running"
-    }})
-    for i in domain:
-        start_brute = DomainBrute(i, domain_id)
-        start_brute.multi_brute()
-    connectiondb(domain_db).update_one({"_id": domain_id}, {"$set": {
-        "status": "Done"
-    }})
-    get_domain_title(domain_id)
-
-
-def get_domain_title(domain_id):
-    pool = Pool(processes=50)
-    result = []
-    for i in connectiondb(subdomain_db).find({"domain_id": domain_id}):
-        result.append(pool.apply_async(get_title, (i['subdomain'], i['_id'])))
-    pool.close()
-    pool.join()
-    for res in result:
-        lock.acquire()
+    def gethost(self, sub_domain):
         try:
-            connectiondb(subdomain_db).update_one({"_id": res.get()["_id"]}, {"$set": {
-                "title": res.get()['title']
-            }})
+            host = socket.gethostbyname(sub_domain)
+            res = {
+                "domain": sub_domain,
+                "host": host,
+            }
+            self.res_tmp.append(res)
         except Exception as e:
-            print("update title error", e)
-        lock.release()
+            # print(e)
+            pass
+
+    def _result_save(self):
+        save_tmp = []
+        for res in self.res_tmp:
+            if self.resolver_domain == res['host']:
+                pass
+            else:
+                data = {
+                    "subdomain": res['domain'],
+                    "domain": self.domain,
+                    "domain_id": self.domain_id,
+                    "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "result": res['host'],
+                    "title": '',
+                }
+                save_tmp.append(data)
+        try:
+            if save_tmp:
+                connectiondb(subdomain_db).insert_many(save_tmp, ordered=True)
+        except Exception as e:
+            print("[!] %s Saved domain result error: %s" % (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), e))
