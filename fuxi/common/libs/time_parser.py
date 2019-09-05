0e678e905fdb454638ef2f579876f4a04855243c
zhangjunfeng01 <zhangjunfeng01@weidian.com>
2019-09-05 16:55:33 +0800

    2019.09.05 xx

--- fuxi/common/libs/time_parser.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author  : jeffzhang
-# @Time    : 2019/4/30
-# @File    : time_parser.py
-# @Desc    : ""
-
-
---
+++ fuxi/common/utils/random_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : jeffzhang
+# @Time    : 2019/1/21
+# @File    : generate_id.py
+# @Desc    : ""
+
+import random
+
+
+def random_str(length=12):
+    seed = "1234567890abcdefghijklmnopqrstuvwxyz"
+    salt = ''.join(random.sample(seed, length))
+    return salt
