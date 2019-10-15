372d0924328fc1a5b1c87ccb3a908849c43c65e1
jeffzh3ng <jeffzh3ng@gmail.com>
2019-10-15 12:00:25 +0800

    2019.10.14 update config.py

--- instance/_config.py
+++ instance/_config.py
@@ -7,27 +7,30 @@
 
 import os
 
+LOGGER_PATH = os.path.abspath(os.path.dirname(__file__)) + '/../logs/'
+
 
 class BaseConfig(object):
     # Base configuration
     DEBUG = False
     AUTH = True
+    SERVER_HOST = "127.0.0.1"
+    SERVER_PORT = 50020
     SECRET_KEY = 'B10ySw1nPL8JBo6z'
-    LOGGER_PATH = os.path.abspath(os.path.dirname(__file__)) + '/../logs/'
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     AUTH = False
     # Redis configuration
-    REDIS_HOST = ""
+    REDIS_HOST = "127.0.0.1"
     REDIS_PORT = ""
     REDIS_PASSWORD = ""
     REDIS_DB = 0
     # MongoDB configuration
-    MONGO_HOST = ''
-    MONGO_PORT = 0
-    MONGO_DB = ''
+    MONGO_HOST = '127.0.0.1'
+    MONGO_PORT = 27017
+    MONGO_DB = 'fuxi_dev'
 
 
 class ProductionConfig(BaseConfig):
