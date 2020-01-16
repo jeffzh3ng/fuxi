7362f9456caaa0e72a01285a00a6838c150d3036
jeffzh3ng <jeffzh3ng@gmail.com>
2020-01-16 21:02:44 +0800

    date: 2020_0116_2102

--- migration/pocs/redis_unauthorized_access.py
+++ migration/pocs/redis_unauthorized_access.py
@@ -17,14 +17,13 @@ class DemoPOC(POCBase):
     createDate = '2015-10-26'
     updateDate = '2015-12-09'
     references = ['http://sebug.net/vuldb/ssvid-89339']
-    name = 'Redis 未授权访问'
+    name = 'Redis Unauthenticated'
     appPowerLink = 'http://redis.io/'
     appName = 'Redis'
     appVersion = 'All'
     vulType = 'Unauthorized access'
     desc = '''
-        redis 默认不需要密码即可访问，黑客直接访问即可获取数据库中所有信息，造成严重的信息泄露。
-        说明：“此版本通过生成公钥写入redis文件后直接运行此脚本可在服务器上/root/.ssh文件下生成公钥”
+        redis unauthenticated code execution
     '''
     samples = ['']
     category = POC_CATEGORY.EXPLOITS.REMOTE
