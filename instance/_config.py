6a90abee90619ab896976e2c8f82aefca6ac2217
jeffzhang <jeffzh3ng@gmail.com>
2020-01-17 23:52:25 +0800

    update: Modify preset configuration

--- instance/_config.py
+++ instance/_config.py
@@ -20,11 +20,9 @@ class BaseConfig(object):
 
 
 class DevelopmentConfig(BaseConfig):
-    DEBUG = True
-    AUTH = False
     # Redis configuration
     REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = "6379"
+    REDIS_PORT = 6379
     REDIS_PASSWORD = ""
     REDIS_DB = 0
     # MongoDB configuration
