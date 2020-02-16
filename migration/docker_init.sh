486e6a8325d317c87509d659b0ba3c4a36c7997d
jeffzhang <jeffzh3ng@gmail.com>
2020-02-16 13:46:17 +0800

    fix: change fuxi log path

--- migration/docker_init.sh
+++ migration/docker_init.sh
@@ -2,11 +2,11 @@
 
 sed -i 's/bind 127.0.0.1 ::1/bind 127.0.0.1/' /etc/redis/redis.conf
 sed -i "s/dbpath=.*/dbpath=\/data\/mongodb/" /etc/mongodb.conf
+cp /opt/fuxi/instance/_config.py /opt/fuxi/instance/config.py
 sed -i "s/LOGGER_PAT.*/LOGGER_PATH=\'\/data\/logs\'/" /opt/fuxi/instance/config.py
 mkdir -p /data/mongodb /data/log /data/logs
 service mongodb restart
 service redis-server restart
-cp /opt/fuxi/instance/_config.py /opt/fuxi/instance/config.py
 cd /opt/fuxi
 nohup python3.7 fuxi_manage.py >> /data/log/fuxi_web.txt 2>&1 &
 nohup celery worker -A fuxi_celery_worker.celery >> /data/log/fuxi_celery.txt 2>&1 &
